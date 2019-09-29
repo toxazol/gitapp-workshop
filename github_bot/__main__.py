f97ceda24faeb1a5c5d2bd68c1adf3fe5a39ab8d
toxazol <tritoncheg@gmail.com>
2019-09-29 17:35:51 +0300

    init git app

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='txzlApp',
+        version='1.0.0',
+        url='https://github.com/apps/txzlApp',
+    )
